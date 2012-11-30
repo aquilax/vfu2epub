3c81a7a774b0e550153958c6d8a59cda765d0bb8
Evgeniy <evgeniy@boxore.com>
2012-11-30 10:17:43 +0200

    Added codepage conversion

--- vfu2epub.sh
+++ vfu2epub.sh
@@ -2,9 +2,12 @@
 
 # unzip the archive
 unzip -o $1 -d ./out
+
 # unzip the archives
 for z in ./out/vfu0*.zip; do unzip -o $z  -d ./out; done
 
+# convert to utf8
+for z in ./out/VFU.0*; do iconv -f MIK -t utf8 $z -o $z.utf8.txt; done
 
 # clean afterwards
 rm ./out/*

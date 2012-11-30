6d82340577bd87499435594c04e8038f94779708
Evgeniy <evgeniy@boxore.com>
2012-11-30 17:50:01 +0200

    Added basic HTML template; Added basic book structure;

--- vfu2epub.sh
+++ vfu2epub.sh
@@ -9,5 +9,8 @@ for z in ./out/vfu0*.zip; do unzip -o $z  -d ./out; done
 # convert to utf8
 for z in ./out/VFU.0*; do iconv -f MIK -t utf8 $z -o $z.utf8.txt; done
 
+# create HTML
+for z in ./out/*.utf8.txt; do sed "9r $z" ./assets/template.html > $z.xhtml; done
+
 # clean afterwards
 rm ./out/*
